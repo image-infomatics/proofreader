2af9660d2e5fcb3cbb8aa79ddc04e7705fa20710
julesberman <julesmichaelberman@gmail.com>
2021-09-22 12:06:08 -0400

    manual training

--- proofreader/model/config.py
+++ proofreader/model/config.py
@@ -4,8 +4,10 @@ from dataclasses import dataclass
 
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.trainer import optimizers
 from torch.utils.data import DataLoader, Subset
 
+import torch
 import torch.nn.functional as F
 
 from proofreader.data.splitter import SplitterDataset
@@ -35,12 +37,14 @@ class DatasetConfig:
     num_points: int = 1024
     batch_size: int = 64
     val_split: float = 0.15
+    epoch_multplier: int = 1
 
 
 @dataclass
 class ModelConfig:
     model: str = 'pointnet'
     loss = str = 'nll'
+    optimizer: str = 'AdamW'
     dim: int = 256
     learning_rate: float = 1e-3
 
@@ -64,6 +68,29 @@ class ExperimentConfig:
         return f'NAME\n{self.name}\nDATASET{d}\nMODEL\n{m}\n'
 
 
+def build_dataset_from_config(dataset_config: DatasetConfig, aug_config: AugmentorConfig, vols):
+
+    # build augmentor
+    augmentor = Augmentor(center=aug_config.center, shuffle=aug_config.shuffle,
+                          rotate=aug_config.rotate, scale=aug_config.scale,
+                          jitter=aug_config.jitter, normalize=aug_config.normalize)
+
+    # build dataset
+    dataset = SplitterDataset(
+        vols, dataset_config.num_slices, dataset_config.radius, dataset_config.context_slices,
+        num_points=dataset_config.num_points, Augmentor=augmentor, open_vol=True, epoch_multplier=dataset_config.epoch_multplier, shuffle=True, torch=True)
+
+    # split into train and val
+    split = math.floor(len(dataset)*dataset_config.val_split)
+    train_split = list(range(split, len(dataset)-1))
+    val_split = list(range(0, split))
+    ds_train = Subset(dataset, train_split)
+    ds_val = Subset(dataset, val_split)
+    print(f'# train: {len(ds_train)}, # val: {len(ds_val)}')
+
+    return ds_train, ds_val
+
+
 def build_dataloader_from_config(dataset_config: DatasetConfig, aug_config: AugmentorConfig, vols):
 
     # build augmentor
@@ -74,7 +101,7 @@ def build_dataloader_from_config(dataset_config: DatasetConfig, aug_config: Augm
     # build dataset
     dataset = SplitterDataset(
         vols, dataset_config.num_slices, dataset_config.radius, dataset_config.context_slices,
-        num_points=dataset_config.num_points, Augmentor=augmentor, open_vol=True, epoch_multplier=5, shuffle=True, torch=True)
+        num_points=dataset_config.num_points, Augmentor=augmentor, open_vol=True, epoch_multplier=dataset_config.epoch_multplier, shuffle=True, torch=True)
 
     # split into train and val
     split = math.floor(len(dataset)*dataset_config.val_split)
@@ -106,6 +133,21 @@ def build_model_from_config(model_config: ModelConfig, dataset_config: DatasetCo
     model = Classifier(backbone=backbone, loss=loss,
                        learning_rate=model_config.learning_rate)
     return model
+
+
+def build_full_model_from_config(model_config: ModelConfig, dataset_config: DatasetConfig):
+    if model_config.loss == 'nll':
+        loss = F.nll_loss
+    elif model_config.loss == 'ce':
+        loss = F.cross_entropy
+
+    if model_config.model == 'pointnet':
+        model = PointNetCls(num_points=dataset_config.num_points)
+    if model_config.optimizer == 'AdamW':
+        optimizer = torch.optim.AdamW(
+            model.parameters(), lr=model_config.learning_rate, betas=(0.9, 0.999), weight_decay=0.05)
+
+    return model, loss, optimizer
 
 
 def build_trainer_from_config(config: TrainerConfig, args):
